7b3f0bc7388b5d16887f2acdae966d4115747450
geleeroyale <krassvs@gmail.com>
2020-04-01 18:23:13 +0200

    update donateinstance to to point to noncon.org/donate

--- src/components/external-sites/DonateInstance.js
+++ src/components/external-sites/DonateInstance.js
@@ -4,7 +4,7 @@ function DonateInstance() {
 	return (
 		<iframe
 			title='Donate to support NONCON 2020'
-			src='https://noncon.org/about'
+			src='https://noncon.org/#donate'
 			style={{
 				height: `100%`,
 				width: `100%`,
