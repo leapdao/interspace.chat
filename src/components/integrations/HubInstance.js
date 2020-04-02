1624f926a62a5405c7373bcaaabcfec914b6af02
geleeroyale <krassvs@gmail.com>
2020-04-03 01:36:27 +0200

    add hub cryptoeconomics lab

--- src/components/integrations/HubInstance.js
+++ src/components/integrations/HubInstance.js
@@ -7,7 +7,7 @@ function HubInstance() {
 			width='100%'
 			height='100%'
 			frameBorder='0'
-			src='https://hubs.mozilla.com/nKCuNrg/house-of-defiance'
+			src='https://hubs.mozilla.com/TsLPctF/cryptoeconomics-lab-day-1'
 			allow='microphone; camera; vr; speaker;'
 		/>
 	);
