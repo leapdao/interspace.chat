b7063ebfda2a6ac3bd4ad81bb5d5fe9a3ef7373b
geleeroyale <krassvs@gmail.com>
2020-04-03 01:27:25 +0200

    add links for youtube

--- src/components/integrations/YoutubeInstance.js
+++ src/components/integrations/YoutubeInstance.js
@@ -2,19 +2,19 @@ import React, { useState, useEffect, useContext } from 'react';
 import { FloatingSpaceContext } from '../../contexts/FloatingSpaceContext';
 
 function YoutubeInstance({ roomData }) {
-	const [youtubeRoom, setYoutubeRoom] = useState('1didILpsp0s');
+	const [youtubeRoom, setYoutubeRoom] = useState('1yXGoPVPk88');
 	const { currentFloatingSpaces } = useContext(FloatingSpaceContext);
 	const space = currentFloatingSpaces;
 
 	useEffect(() => {
 		if (space.indexOf('parallel-society') > -1) {
-			setYoutubeRoom('80mwsRuuTmM');
+			setYoutubeRoom('1yXGoPVPk88');
 		} else if (space.indexOf('cryptoeconomics-lab') > -1) {
-			setYoutubeRoom('2PP9qIVRDe0');
+			setYoutubeRoom('GpwK_lHYv3Q');
 		} else if (space.indexOf('metatrack') > -1) {
-			setYoutubeRoom('7LZbQZ0hcK4');
+			setYoutubeRoom('2oSUnUpqg3E');
 		} else {
-			setYoutubeRoom('80mwsRuuTmM');
+			setYoutubeRoom('1yXGoPVPk88');
 		}
 		return console.log(youtubeRoom);
 	}, [space, youtubeRoom]);
